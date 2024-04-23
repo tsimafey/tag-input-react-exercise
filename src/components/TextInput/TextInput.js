94afc270ea87c9fc4c36b74a999d8c099c0ea79c
tsimafey <tsimafey1401@gmail.com>
2024-04-23 11:45:43 +0200

    Add styles for text input

---
+++ src/components/TextInput/TextInput.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styles from './TextInput.module.css';
+
+export function TextInput({
+  placeholder,
+  searchValue,
+  setSearchValue,
+  textInputStyles,
+}) {
+  return (
+    <div className={styles.textInputContainer}>
+      <input
+        type="text"
+        placeholder={placeholder}
+        aria-label={placeholder}
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+        className={styles.textInput}
+        style={textInputStyles}
+      />
+      {searchValue ? (
+        <div className={styles.clearButtonWrapper}>
+          <button className={styles.clearButton} onClick={() => null}></button>
+        </div>
+      ) : (
+        false
+      )}
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import styles from './index.module.css';
 
-import { TextInput } from './components/TextInput';
+import { TextInput } from './components/TextInput/TextInput';
 import { Suggestions } from './components/Suggestions';
 import { Tag } from './components/Tag';
 
 import { sortTagsBySearchValue } from './utils/sortTagsBySearchValue';
 
-export function TagInput({ allTags, placeholder }) {
+export function TagInput({ allTags, placeholder, textInputStyles }) {
   const [searchValue, setSearchValue] = useState('');
   const [chosenTags, setChosenTags] = useState([]);
   const [sortedTags, setSortedTags] = useState(allTags);
@@ -24,6 +24,7 @@ export function TagInput({ allTags, placeholder }) {
         placeholder={placeholder}
         searchValue={searchValue}
         setSearchValue={setSearchValue}
+        textInputStyles={textInputStyles}
       />
       <div>
         {searchValue ? (
